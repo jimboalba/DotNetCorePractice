c4fbfaa9ff15ec99d6b877a3708e22b1cb1489f2
Jimbo Alba <jimbo.alba@gmail.com>
2020-06-21 20:43:21 +0400

    Added logging project

---
+++ logging/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace logging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
